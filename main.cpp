12e040699b37d6a4f053828178a4f8404bc3726f
Manas Choure <226876218+Manas10k1@users.noreply.github.com>
2026-08-09 16:44:36 +0530

    Update system heading

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(){
     int choice;
 
     do {
-        cout << "\n========== STUDENT MANAGEMENT SYSTEM ==========\n";
+        cout << "\n========== STUDENT MANAGEMENT SYSTEM V1 ==========\n";
         cout << "1. Add Student\n";
         cout << "2. Display All Students\n";
         cout << "3. Insert Student\n";
